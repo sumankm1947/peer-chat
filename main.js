4ab5d7e5f0faa577faf1b5a13b0c802f672cff22
superbsuman <sumanmandal.superb@gmail.com>
2022-09-05 21:36:17 +0530

    update camera permission to get front camera

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ const constraints = {
   video: {
     width: { min: 640, ideal: 1920, max: 1920 },
     height: { min: 480, ideal: 1080, max: 1080 },
+    facingMode: "user"
   },
   audio: true,
 };
